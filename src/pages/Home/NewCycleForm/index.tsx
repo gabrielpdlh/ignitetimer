523a8b519fc4770ebc3541f3457bfc2c37dd57a3
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2025-03-24 21:45:06 -0300

    feat: create a component to create a new cycle

---
+++ src/pages/Home/NewCycleForm/index.tsx
@@ -0,0 +1,44 @@
+import { FormContainer, MinutesAmountInput, TaskInput } from "./styles";
+import { useContext } from "react";
+import { CyclesContext } from "..";
+import { useFormContext } from "react-hook-form";
+
+
+export function NewCycleForm() {
+  const {activeCycle} = useContext(CyclesContext)
+  const {register} = useFormContext()
+  
+
+return (
+  
+  <FormContainer>
+    <label htmlFor="tasks">Vou trabalhar em</label>
+    <TaskInput
+      id="task"
+      list="tasks-suggestions"
+      placeholder="Dê um nome para o seu projeto"
+      disabled={!!activeCycle}
+      {...register("task")}
+    />
+    <datalist id="tasks-suggestions">
+      <option value="Projeto 1" />
+      <option value="Projeto 2" />
+      <option value="Projeto 3" />
+    </datalist>
+
+    <label htmlFor="minutesAmount">durante</label>
+    <MinutesAmountInput
+      placeholder="00"
+      type="number"
+      id="minutesAmount"
+      step={5}
+      min={1}
+      max={60}
+      disabled={!!activeCycle}
+      {...register("minutesAmount", { valueAsNumber: true })}
+    />
+
+    <span>minutos.</span>
+  </FormContainer>
+)
+}
